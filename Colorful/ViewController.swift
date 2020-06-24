2f18820b1b77e33e72ebfed1d2571bfa13e48bfb
zackradisic <zack@theradisic.com>
2020-06-23 21:03:49 -0400

    Initial Commit

---
+++ Colorful/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Colorful
+//
+//  Created by Zack Radisic on 2020-06-23.
+//  Copyright © 2020 Zack Radisic. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
